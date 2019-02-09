0f7182c2a46e0f0553f686ebe55a6fdcd06bffc6
naveen-pete <naveen.shenoy.blr@gmail.com>
2019-02-09 09:22:57 +0530

    commit changes to api js

--- apps/day-01/blogger/src/api/BloggerApi.js
+++ apps/day-01/blogger/src/api/BloggerApi.js
@@ -1,29 +1,6 @@
-const baseApiUrl = 'http://localhost:3001';
+const apiUrl = 'http://localhost:3001/posts';
 
-export const getCategories = () =>
-   fetch(`${baseApiUrl}/categories`).then(response => response.json());
-
-export const getPosts = () =>
-   fetch(`${baseApiUrl}/posts`).then(response => response.json());
-
-export const getPost = id =>
-   fetch(`${baseApiUrl}/posts/${id}`).then(response => response.json());
-
-export const addPost = post =>
-   fetch(`${baseApiUrl}/posts`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(post)
-   }).then(response => response.json());
-
-export const updatePost = post =>
-   fetch(`${baseApiUrl}/posts/${post.id}`, {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(post)
-   }).then(response => response.json());
-
-export const deletePost = id =>
-   fetch(`${baseApiUrl}/posts/${id}`, {
-      method: 'DELETE'
-   }).then(response => response.json());
+export const getPosts = () => {
+   return fetch(apiUrl)
+      .then(response => response.json());
+};
